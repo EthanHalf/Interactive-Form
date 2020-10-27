c8b88c481ce03802e71367b003b94c44977a2634
Ethan Halfhide <ethan.halfhide@gmail.com>
2020-10-27 13:46:09 -0400

    commit

---
+++ interactive-form-v2/js/script.js
@@ -0,0 +1,9 @@
+
+
+const nameInput = document.getElementById('name');
+
+const otherJobRole = document.getElementById('other-title');
+
+//let colorPlaceholderText = document.getElementById('color');
+
+//colorPlaceholderText.placeholder = "Please select a T-shirt theme";
