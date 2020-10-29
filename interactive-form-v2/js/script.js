524cedac44b9bceefea431c5ea3619a4fe456dc7
Ethan Halfhide <ethan.halfhide@gmail.com>
2020-10-29 12:36:54 -0400

    eventListener commit

--- interactive-form-v2/js/script.js
+++ interactive-form-v2/js/script.js
@@ -1,9 +1,31 @@
 
 
 const nameInput = document.getElementById('name');
-
+const designDropDown = document.getElementById('design')
 const otherJobRole = document.getElementById('other-title');
+//hides other-title field
+otherJobRole.setAttribute('type', 'hidden')
+//gets the color id from select element
+let colorSelectElement = document.getElementById('color');
+//creates a new option and defines what it will say
+//https://www.w3schools.com/jsref/met_select_add.asp
+let option = document.createElement("option");
+option.text = "Please select a T-shirt theme";
+//adds "Please select a T-shirt theme" to the list
+colorSelectElement.add(option);
+//display the option as default
 
-//let colorPlaceholderText = document.getElementById('color');
 
-//colorPlaceholderText.placeholder = "Please select a T-shirt theme";
+//design event listner for js puns
+designDropDown.addEventListener('click', (e) => {
+    //hide colors in Color select menu
+    if (designDropDown[0].selected = true) {
+        colorSelectElement[0-2].hidden = true;
+    }else if (designDropDown[1].selected = true) {
+        colorSelectElement[3-5].hidden = true;
+    //function hideColors(value){
+    //$('select option[value="'+value+'"]').appendTo('#optionHolder');}
+    //function showOption(value){
+   //$('#optionHolder option[value="'+value+'"]').appendTo('#selectID');}
+});
+
